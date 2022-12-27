db108d67e7bb6c431d9eeb0a2a07ac70ee5b5b8e
Jott4c <jrcba123@gmail.com>
2022-12-26 21:09:24 -0300

    init 2

--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -13,50 +13,9 @@ const Contact = () => {
 				<Main>
 					<Dev>
 						<figure>
-							<img className="dev-photo" src={Gabs} alt="Gabs" />
-						</figure>
-						<h3 className="dev-name">Gabs Ghidini</h3>
-						<div className="dev-bio">
-							<h4>Quality Assurance & UI Designer</h4>
-							Atuando como QA e UI Designer, Gabs projetou a parte
-							visual do site além de ter colaborado com análise de
-							códigos para garantir sempre escalabilidade e
-							qualidade.
-						</div>
-						<a
-							className="linkedin"
-							href="https://www.linkedin.com/in/gabsghidini/"
-						>
-							<BsLinkedin size={25} />
-						</a>
-					</Dev>
-					<Dev>
-						<figure>
 							<img
 								className="dev-photo"
-								src={Jasmyne}
-								alt="Jasmyne Leandro"
-							/>
-						</figure>
-						<h3 className="dev-name">Jasmyne Leandro</h3>
-						<div className="dev-bio">
-							<h4>Product Owner</h4>
-							Atuando como uma representante do cliente, Jasmyne
-							ficou responsável pela gestão do que a Last Steps
-							ofereceria aos seus usuários finais.
-						</div>
-						<a
-							className="linkedin"
-							href="https://www.linkedin.com/in/jasmynelsx/"
-						>
-							<BsLinkedin size={25} />
-						</a>
-					</Dev>
-					<Dev>
-						<figure>
-							<img
-								className="dev-photo"
-								src="https://ca.slack-edge.com/TQZR39SET-U035HRV95R9-7057e270805a-512"
+								src=""
 								alt="Junior Alencar"
 							/>
 						</figure>
@@ -76,29 +35,7 @@ const Contact = () => {
 							<BsLinkedin size={25} />
 						</a>
 					</Dev>
-					<Dev>
-						<figure>
-							<img
-								className="dev-photo"
-								src={Giovani}
-								alt="Giovani Salvador"
-							/>
-						</figure>
-						<h3 className="dev-name">Giovani Salvador</h3>
-						<div className="dev-bio">
-							<h4>Scrum Master</h4>
-							Responsável por garantir que o Time Scrum se
-							orientasse pelos valores e práticas do Scrum, além
-							de garantir que os objetivos estavam claros e
-							conforme o tempo esperado de desenvolvimento.
-						</div>
-						<a
-							className="linkedin"
-							href="https://www.linkedin.com/in/giovani-salvador/"
-						>
-							<BsLinkedin size={25} />
-						</a>
-					</Dev>
+
 				</Main>
 			</Body>
 			<Footer>
